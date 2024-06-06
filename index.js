c86a50c4014c1683935900bf5a02492b136da409
EnderHostXDXD <enderhosting00@gmail.com>
2024-06-06 16:32:29 -0400

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Tienda https://enderhosting.com.mx/","EnderHosting"];
 
 
 let currentIndex = 0;
